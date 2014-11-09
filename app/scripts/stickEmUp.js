b37b47d97a87e58bb91341228b34cfe674718ed7
Ben Lodge <benrlodge@gmail.com>
2014-11-09 09:57:22 -0500

    get basics in place

--- app/scripts/stickEmUp.js
+++ app/scripts/stickEmUp.js
@@ -1,12 +1,3 @@
-log = function(m){console.log(m);}
-
-log('stickemup');
-
-
-
-// #intro
-
-
 /*!
  * stickEmUp!
  * Original author: @benrlodge
@@ -15,39 +6,120 @@ log('stickemup');
 
 ;(function ( $, window, document, undefined ) {
 
+    'use strict';
+
+    // Defaults
     var pluginName = 'stickEmUp',
         defaults = {
-            stickOffset: 0
+            stickyClass: 'stickEmUp',
+            el: '',
+            offset: 0,
+            stickOffset: 0,
+            stickAtEl: '',
+            callback: ''
         };
 
     // The actual plugin constructor
     function Plugin( element, options ) {
         this.element = element;
-
-        // jQuery has an extend method that merges the 
-        // contents of two or more objects, storing the 
-        // result in the first object. The first object 
-        // is generally empty because we don't want to alter 
-        // the default options for future instances of the plugin
-        this.options = $.extend( {}, defaults, options) ;
-        
+        this.options = $.extend( {}, defaults, options);
         this._defaults = defaults;
         this._name = pluginName;
-        
         this.init();
     }
 
-    Plugin.prototype.init = function () {
-        // Place initialization logic here
-        // You already have access to the DOM element and
-        // the options via the instance, e.g. this.element 
-        // and this.options
-        log('oh we init this shit@!!');
-        log(this.options);
+
+    // Methods
+    Plugin.prototype = {
+        sticky: false,
+        elCachedPosition: '',
+        pastOffset: 0,
+        direction: '',
+
+        theStateOfThings: function(){
+            var dos = this.docOffset();
+            var eos = this.elOffset();
+            var dir = this.direction;
+
+            return {
+                docOffset: dos,
+                elOffset: eos,
+                direction: dir
+            };
+        },
+
+        docOffset: function() {
+            return $(document).scrollTop();
+        },
+
+        elOffset: function() {
+            return $(this.element).offset().top;
+        },
+    
+        stick: function() {
+            if(this.sticky){ return; }
+            console.log('stick');
+
+            $(this.element).addClass(this.options.stickyClass);
+            this.sticky = true;
+        },
+
+        unstick: function() {
+            if(!this.sticky){ return; }
+            console.log('UNstick');
+            $(this.element).removeClass(this.options.stickyClass);
+            this.sticky = false;
+        },
+
+
+        cachePositions: function(){
+            this.elCachedPosition = this.elOffset();
+        },
+  
+        checkPositions: function(){
+
+            var docOff = this.docOffset();
+            
+            // set direction
+            this.direction = this.pastOffset ? 'down' : 'up';
+            this.pastOffset = docOff;
+
+            // check stickiness
+            if (docOff >= this.elCachedPosition){
+                this.stick();
+            }
+            else{
+                this.unstick();
+            }
+
+
+            // send callback
+            var things = this.theStateOfThings();
+            if (typeof this.options.callback === 'function') { // make sure the callback is a function
+                this.options.callback(things);
+            }
+
+        },
+
+        events: function(){
+            var scope;
+            scope = this;
+            $(window).scroll(function() {
+                scope.checkPositions();
+            });
+
+        },
+
+        init: function(){
+            console.log(this.options);
+            this.cachePositions();
+            this.events();
+        }
     };
 
-    // A really lightweight plugin wrapper around the constructor, 
-    // preventing against multiple instantiations
+
+
+    // Build the Plugin
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
@@ -55,7 +127,7 @@ log('stickemup');
                 new Plugin( this, options ));
             }
         });
-    }
+    };
 
 })( jQuery, window, document );
 
@@ -63,8 +135,52 @@ log('stickemup');
 
 
 
-$('#intro').stickEmUp({
-	stickClass: 'farts'
-});
+
+
+
+
+
+
+
+
+
+   // cachePositions: function() {
+
+   //          // var scope = this;
+   //          // this.cachedOffset = this.elementOffset();
+
+   //          // if (this.options.absoluteAtEl) {
+                
+   //          //     this.cachedAbsoluteOffset = this.absoluteElOffset() - this.options.absoluteAtOffset;
+   //          //     return $(window).load(function() {
+   //          //     return scope.cachedAbsoluteOffset = scope.absoluteElOffset() - scope.absoluteAtOffset;
+   //          //     });
+   //          // }
+
+   //      },
+
+
+
+
+        // checkPositions: function(){
+
+        //     console.log('Window Position:  ' + this.docOffset());
+
+
+
+        //     if (this.docOffset() >= this.cachedOffset) {
+        //       this.stickEmUp();
+        //       if (this.options.absoluteAtEl) {
+        //         if (this.docOffset() >= this.cachedAbsoluteOffset) {
+        //           this.stickEmAbsolute();
+        //         } else {
+        //           this.stickEmDownAbsolute();
+        //         }
+        //       }
+        //     }
+        //     if (this.docOffset() <= this.cachedOffset) {
+        //       return this.stickEmDown();
+        //     }
+        // },
 
 
